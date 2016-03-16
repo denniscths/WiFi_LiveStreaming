6aec8b2821f95759dcb74ab7bcc2eb351dcea16e
dennis <denniscths@gmail.com>
2016-03-16 14:55:31 +0800

    WiFi_LiveStreaming_Test

---
+++ VideoStreamingTest1/app/src/main/java/example/videostreamingtest1/AppConfig.java
@@ -0,0 +1,7 @@
+package example.videostreamingtest1;
+
+public class AppConfig {
+    public static final String STREAM_URL = "rtsp://192.168.1.104:1935/live/myStream";
+    public static final String PUBLISHER_USERNAME = "dennis3";
+    public static final String PUBLISHER_PASSWORD = "123456";
+}
